603812f310ff085de4b3dcf48f261f89e413a0bc
Wolfleader <james.morgan@iinet.net.au>
2020-02-05 18:22:46 +0800

    Add project files.

---
+++ DinerMax3000BusinessLayer/DrinkMenu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinerMax3000.Business
+{
+	public class DrinkMenu : Menu
+	{
+		public string Disclaimer;
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DinerMax3000
+ A solution of all my Diner Max projects
