4323f1492fffa0899a18bef114d48bea95d37285
Wolfleader <james.morgan@iinet.net.au>
2020-02-11 20:11:05 +0800

    view model

---
+++ DinerMax3000WPFClient/DinerMax3000ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinerMax3000.Business;
+
+namespace DinerMax3000.WPFClient
+{
+	public class DinerMax3000ViewModel
+	{
+		public List<Menu> AllMenus
+		{
+			get
+			{
+				return Menu.GetAllMenus();
+			}
+		}
+	}
+}
